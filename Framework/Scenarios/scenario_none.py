968d674e06c8b5b03978e5755562e9c08bd7ef2b
Anna Meszaros <44446049+anna-meszaros@users.noreply.github.com>
2023-07-14 14:01:58 +0200

    Update scenario_none.py

--- Framework/Scenarios/scenario_none.py
+++ Framework/Scenarios/scenario_none.py
@@ -2,25 +2,25 @@
 class scenario_none():
     def __init__(self):
         pass
-    
-    def give_default_classification(self = None):
-        return 'normal'
-    
-    def give_classifications(self = None):
-        Class = {'normal': 0}
-        return Class, 1
-        
+            
     def get_name(self = None):
         return 'No specific scenario'
         
-    def can_provide_general_input(self = None):
-        return None
-        
-    def pov_agent(self = None):
-        return None
+    def give_classifications(self = None):
+        Class = {'normal': 0}
+        return Class, 1
+    
+    def give_default_classification(self = None):
+        return 'normal'
         
     def classifying_agents(self = None):
         return ['tar']
-    
+            
+    def pov_agent(self = None):
+        return None
+
+    def can_provide_general_input(self = None):
+        return None
+
     def calculate_safe_action(self, D_class, t_D_class, data_set, path, t, domain):
         return None
