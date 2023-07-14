9da7f8081f3076daeede4a309c993dcbd077ad9f
Anna Meszaros <44446049+anna-meszaros@users.noreply.github.com>
2023-07-14 13:29:32 +0200

    Update ADE.py
    Removed main_result_idx

--- Framework/Evaluation_metrics/ADE.py
+++ Framework/Evaluation_metrics/ADE.py
@@ -31,10 +31,7 @@ class ADE(evaluation_template):
 
         E = Error / Samples 
         return [E.mean()]
-    
-    def main_result_idx(self = None):
-        return 0
-    
+        
     def get_output_type(self = None):
         return 'path_all_wi_pov'
     
