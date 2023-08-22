Smal fix to normalization in trajflow

Framework/Models/model_template.py
@@ -468,7 +468,82 @@ class model_template():
                 if not Pred_agents[i,j]:
                     continue
                 self.Output_path_pred.iloc[i_sample][agent] = Pred[i, j,:, :, :].astype('float32')
-                
+    
+    def provide_all_training_trajectories(self):
+        r'''
+        This function provides trajectroy data an associated metadata for the training of model
+        during prediction and training. It returns the whole training set (including validation set)
+        in one go
+
+
+        Returns
+        -------
+        X : np.ndarray
+            This is the past observed data of the agents, in the form of a
+            :math:`\{N_{samples} \times N_{agents} \times N_{I} \times 2\}` dimensional numpy array with float values. 
+            If an agent is fully or or some timesteps partially not observed, then this can include np.nan values.
+        Y : np.ndarray, optional
+            This is the future observed data of the agents, in the form of a
+            :math:`\{N_{samples} \times N_{agents} \times N_{O} \times 2\}` dimensional numpy array with float values. 
+            If an agent is fully or or some timesteps partially not observed, then this can include np.nan values. 
+            This value is not returned for **mode** = *'pred'*.
+        T : np.ndarray
+            This is a :math:`\{N_{samples} \times N_{agents}\}` dimensional numpy array. It includes strings that indicate
+            the type of agent observed (see definition of **provide_all_included_agent_types()** for available types).
+            If an agent is not observed at all, the value will instead be '0'.
+        img : np.ndarray
+            This is a :math:`\{N_{samples} \times N_{agents} \times H \times W \times C\}` dimensional numpy array. 
+            It includes uint8 integer values that indicate either the RGB (:math:`C = 3`) or grayscale values (:math:`C = 1`)
+            of the map image with height :math:`H` and width :math:`W`. These images are centered around the agent 
+            at its current position, and are rotated so that the agent is right now driving to the right. 
+            If an agent is not observed at prediction time, 0 values are returned.
+        img_m_per_px : np.ndarray
+            This is a :math:`\{N_{samples} \times N_{agents}\}` dimensional numpy array. It includes float values that indicate
+            the resolution of the provided images in *m/Px*. If only black images are provided, this will be np.nan. 
+            Both for **Y**, **img** and 
+        Pred_agents : np.ndarray
+            This is a :math:`\{N_{samples} \times N_{agents}\}` dimensional numpy array. It includes boolean value, and is true
+            if it expected by the framework that a prediction will be made for the specific agent.
+            
+            If only one agent has to be predicted per sample, for **Y**, **img** and **img_m_per_px**, :math:`N_{agents} = 1` will
+            be returned instead, and the agent to predicted will be the one mentioned first in **X** and **T**.
+        Sample_id : np.ndarray, optional
+            This is a :math:`N_{samples}` dimensional numpy array with integer values. Those indicate from which original sample
+            in the dataset this sample was extracted. This value is only returned for **mode** = *'pred'*.
+        Agent_id : np.ndarray, optional
+            This is a :math:`\{N_{samples} \times N_{agents}\}` dimensional numpy array with integer values. Those indicate from which 
+            original agent in the dataset this agent was extracted.. This value is only returned for **mode** = *'pred'*.
+
+        '''
+        
+        assert self.get_output_type()[:4] == 'path'
+        if not self.extracted_data:
+            self.prepare_batch_generation()
+        
+        I_train = self._extract_useful_training_samples()
+        
+        X_train = self.X[I_train]
+        Y_train = self.Y[I_train]
+        T_train = self.T[I_train]
+        Pred_agents_train = self.Pred_agents[I_train]
+        
+        if self.img is not None:
+            img_train = self.img[I_train]
+            img_m_per_px_train = self.img_m_per_px[I_train]
+        else:
+            img_train = None
+            img_m_per_px_train = None
+        
+        Sample_id_train = self.ID[I_train,0,0]
+        Agents = np.array(self.input_names_train)
+        Agent_id_train = Agents[self.ID[I_train,:,1]]
+        
+        
+        return [X_train, Y_train, T_train, img_train, img_m_per_px_train, 
+                Pred_agents_train, Sample_id_train, Agent_id_train]
+        
+        
+    
     
     def provide_batch_data(self, mode, batch_size, val_split_size = 0.0, ignore_map = False):
         r'''

Framework/Models/trajflow_meszaros.py
@@ -76,6 +76,7 @@ class trajflow_meszaros(model_template):
         
     
     def extract_batch_data(self, X, T, Y = None, img = None):
+        
         # Get type of agents
         T_out = T.astype(str)
         Ped_agents = T_out == 'P'
@@ -85,14 +86,18 @@ class trajflow_meszaros(model_template):
         T_out = np.fromstring(T_out.reshape(-1), dtype = np.uint32).reshape(*T_out.shape, int(str(T_out.astype(str).dtype)[2:])).astype(np.uint8)[:,:,0]
         T_out = torch.from_numpy(T_out).to(device = self.device)
         
-        # TODO: Think about moving the application of the standardization to somewhat later
-        # TODO: There might be problems with the distance calculation in the scene encoder
+        # Normalize positions
+        X = (X - self.min_pos) / (self.max_pos - self.min_pos)
+        
         # Standardize positions
         X[Ped_agents]  /= self.std_pos_ped
         X[~Ped_agents] /= self.std_pos_veh
         X = torch.from_numpy(X).float().to(device = self.device)
         
         if Y is not None:
+            # Normalize future positions
+            Y = (Y - self.min_pos) / (self.max_pos - self.min_pos)
+            
             # Standardize future positions
             Y[Ped_agents[:,0]]  /= self.std_pos_ped
             Y[~Ped_agents[:,0]] /= self.std_pos_veh
@@ -100,13 +105,6 @@ class trajflow_meszaros(model_template):
         
         if img is not None:
             img = torch.from_numpy(img).float().to(device = self.device) / 255
-
-        if self.data_set.get_name()['file'] == 'Fork_P_Aug':
-            traj_tmp = torch.concat((X[:,0], Y[:,0]), dim = 1)
-            self.max_pos = torch.max(traj_tmp)
-            self.min_pos = torch.min(traj_tmp)
-            X = (X - self.min_pos) / (self.max_pos - self.min_pos)
-            Y = (Y - self.min_pos) / (self.max_pos - self.min_pos)
             
         return X, T_out, Y, img
     
@@ -411,15 +409,27 @@ class trajflow_meszaros(model_template):
         T_all = self.provide_all_included_agent_types().astype(str)
         T_all = np.fromstring(T_all, dtype = np.uint32).reshape(len(T_all), int(str(T_all.astype(str).dtype)[2:])).astype(np.uint8)[:,0]
         
+        # Prepare stuff for Normalization
+        if self.data_set.get_name()['file'] == 'Fork_P_Aug':
+            X, Y, _, _, _, _, _, _ = self.provide_all_training_trajectories()
+            traj_tar = np.concatenate((X[:,0], Y[:,0]), dim = 1)
+            self.max_pos = torch.max(traj_tar)
+            self.min_pos = torch.min(traj_tar)
+        else:
+            self.min_pos = 0.0
+            self.max_pos = 1.0
+        
         # Train model components        
         self.fut_model = self.train_futureAE()
         self.flow_dist = self.train_flow(self.fut_model, T_all)
         
         # save weigths 
-        self.weights_saved = []
+        self.weights_saved = [self.min_pos, self.max_pos]
         
         
     def load_method(self):
+        self.min_pos, self.max_pos = self.weights_saved
+        
         fut_model_file = self.model_file[:-4] + '_AE'
         flow_dist_file = self.model_file[:-4] + '_NF'
         self.fut_model = pickle.load(open(fut_model_file, 'rb'))