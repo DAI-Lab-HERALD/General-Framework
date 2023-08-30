030d72fe2778883bbc8a3a628df89dee4cf84cb7
anna-meszaros <ana.mesaros@gmail.com>
2023-08-30 16:59:11 +0200

    Committing changes from this morning

--- Framework/Data_sets/Lyft_interactive.py
+++ Framework/Data_sets/Lyft_interactive.py
@@ -7,8 +7,11 @@ import os
 from data_set_template import data_set_template
 from scenario_none import scenario_none
 
-import trajdata
 from trajdata import UnifiedDataset
+from trajdata.data_structures.scene_metadata import Scene
+from trajdata.simulation import SimulationScene
+from trajdata.data_structures.agent import AgentType
+from trajdata.data_structures.scene import SceneTimeAgent
 # from trajdata import AgentBatch, UnifiedDataset, MapAPI, VectorMap
 
 
@@ -16,6 +19,9 @@ class Lyft_interactive(data_set_template):
     '''
     The Lyft dataset is recored by an AV driving around a city, including gneral
     human bahavior in various situations.
+
+    This dataset is imported using the trajflow libary, which require that numpy >= 1.20.0
+    and numpy <= 1.20.3 is installed.
     
     The data can be found at https://woven-planet.github.io/l5kit/dataset.html
     amd the following citation can be used:
@@ -37,7 +43,7 @@ class Lyft_interactive(data_set_template):
     def include_images(self = None):
         return True
     
-    def get_scenario(self):
+    def set_scenario(self):
         self.scenario = scenario_none()
         
     def create_path_samples(self):
@@ -49,12 +55,61 @@ class Lyft_interactive(data_set_template):
         self.Domain_old = []
         
         
-        data_path = self.path + os.sep + 'Lyft' + os.sep + 'data' + os.sep
+        data_path = self.path + os.sep + 'Data_sets' + os.sep + 'Lyft' + os.sep + 'data'
+        zarr_path = data_path + os.sep + 'scenes' + os.sep + 'sample.zarr'
         
         dataset = UnifiedDataset(desired_data=["lyft_sample"],
-                                 data_dirs={"lyft_sample": data_path})
-        
-        assert False
+                                 data_dirs={"lyft_sample": zarr_path})
+        # Go over scenes
+        for i, scene in enumerate(dataset.scenes()):
+            print('Scene ' + str(i + 1) + ': ' + scene.name)
+            print('Number of frames: ' + str(scene.length_timesteps) + ' (' + str(scene.length_seconds()) + 's)')
+            
+            # Prepare new scene data
+            path = pd.Series(np.zeros(0, np.ndarray), index = [])
+            agent_types = pd.Series(np.zeros(0, str), index = [])
+
+            scene_data_path = dataset._scene_index[i]
+
+            
+            sim_scene = SimulationScene(env_name = scene.env_name,
+                                        scene_name = scene.name,
+                                        scene = scene,
+                                        dataset = dataset,
+                                        init_timestep = 0,
+                                        )
+            scene_data = sim_scene.cache.scene_data_df[['x', 'y']]
+            # Get timesteps
+            t = np.arange(scene.length_timesteps) * scene.dt
+            
+            for agent in scene.agents:
+                if agent.type == AgentType.UNKNOWN:
+                    continue
+                print('Agent ' + str(agent.name) + ': ' + str(agent.type))
+                
+                if agent.name == 'ego':
+                    path_name = 'tar'
+                else:
+                    path_name = 'v_' + str(agent.name)
+
+                traj = np.ones((scene.length_timesteps, 2)) * np.nan
+                traj[agent.first_timestep:agent.last_timestep + 1] = scene_data.loc[agent.name].to_numpy()
+                path[path_name] = traj
+                if agent.type == AgentType.VEHICLE:
+                    agent_types[path_name] = 'V'
+                elif agent.type == AgentType.PEDESTRIAN:
+                    agent_types[path_name] = 'P'
+                elif agent.type == AgentType.MOTORCYCLE:
+                    agent_types[path_name] = 'M'
+                elif agent.type == AgentType.BICYCLE:
+                    agent_types[path_name] = 'B'
+                else:   
+                    raise ValueError('Unknown agent type: ' + str(agent.type))
+                
+                
+
+                    
+
 
         
         
