97ce00934d8395f6a3d2cf2adae84e5d288f4b1d
Julian Schumann <julian.f.schumann@web.de>
2023-11-05 22:51:32 +0100

    Add additional clustering method

--- Multimodal_Eval/Prob_function.py
+++ Multimodal_Eval/Prob_function.py
@@ -7,6 +7,158 @@ from sklearn.mixture import GaussianMixture
 from sklearn.neighbors import BallTree
 from sklearn.decomposition import PCA
 import scipy as sp
+from scipy.sparse.csgraph import minimum_spanning_tree 
+from scipy import sparse
+
+
+def DBCV_multiple_clusterings(X, clusterings, optics = None):
+    num_samples, num_features = X.shape
+    assert len(X.shape) == 2
+    assert len(clusterings.shape) == 2
+
+    # each row in clusterings is a different approach
+    assert clusterings.shape[0] == num_samples
+
+    # Get number of noise samples
+    Num_noise_samples = (clusterings == -1).sum(0)
+    Num_clusters = clusterings.max(0) + 1
+
+    # Get valid clusterings
+    useful = (Num_clusters + (Num_noise_samples > 0).astype(int)) > 1
+
+    # get distances
+    Dist = np.sqrt(((X[:, np.newaxis] - X[np.newaxis]) ** 2).sum(-1))
+
+    # Get core distances
+    Dist_sorted = np.sort(Dist, axis = 1)[:,1:optics.min_samples * 2]
+    adist = (Dist_sorted ** (-1 * num_features)).mean(1) ** (-1 / num_features)
+
+    # Get reachability
+    Adist = np.maximum(adist[:, np.newaxis], adist[np.newaxis])
+    Dreach = np.maximum(Dist, Adist)
+
+    # Intialize best score (worst score of valid clustering is -1)
+    values = -1.1 * np.ones(clusterings.shape[1])
+
+    for i in range(clusterings.shape[1]):
+        if not useful[i]:
+            continue
+        
+        # Get cluster spread
+        test_labels = clusterings[:,i].copy()
+        test_clusters, test_size = np.unique(test_labels, return_counts = True)
+        
+        # Remove noise samples
+        if test_clusters[0] == -1:
+            test_clusters = test_clusters[1:]
+            test_size = test_size[1:]
+
+        num_clusters = len(test_clusters)
+
+        # Get internal DSC (Density Sparseness of a Cluster)
+        DSC = np.ones(num_clusters)
+        # Get external DSPC (Density Separation of a Pair of Clusters)
+        DSPC = np.ones(num_clusters)
+        for label in test_clusters:
+            cluster_samples = test_labels == label
+            Dreach_cluster = Dreach[cluster_samples]
+
+            # Get internal connections
+            Dreach_int = Dreach_cluster[:, cluster_samples]
+
+            # Get internal dispersion
+            max_span_tree = minimum_spanning_tree(sparse.csr_matrix(Dreach_int)).data.max()
+            max_dist = Dist[cluster_samples][:, cluster_samples].max()
+            DSC[label] = min(max_span_tree, max_dist)
+
+            # Get external non noise points
+            external = (test_labels >= 0) & (~cluster_samples)
+
+            if external.any():
+                # Get external connections
+                Dreach_ext = Dreach_cluster[:, external]
+
+                # Get DSCP min 
+                DSPC[label] = Dreach_ext.min()
+            else:
+                DSPC[label] = DSC[label]
+
+        # Get cluster validity
+        CV = (DSPC - DSC) / np.maximum(np.maximum(DSC, DSPC), 1e-6)
+
+        # Get weighted average
+        values[i] = (CV * test_size).sum() / num_samples
+
+        if False:
+            import matplotlib.pyplot as plt
+            import seaborn as sns
+
+            colors = sns.color_palette("husl", Num_clusters[i])
+            colors.append((0.0, 0.0, 0.0))
+
+            data_colors = [colors[j] for j in clusterings[:,i]]
+
+            if optics is not None:
+                data_colors_ordered = [data_colors[j] for j in optics.ordering_]
+                reachability = optics.reachability_[optics.ordering_]
+                reachability[0] = reachability[1]
+
+                plt.figure()
+                plt.plot(np.arange(num_samples), adist[optics.ordering_], c = 'k')
+                for j in range(num_samples - 1):
+                    plt.plot([j, j + 1], [reachability[j], reachability[j + 1]], c = data_colors_ordered[j])
+                plt.show()
+
+            plt.figure()
+            plt.scatter(X[:,0], X[:,1], s = 1, c = data_colors, alpha = 0.9)
+            plt.axis('equal')
+            plt.show()
+
+
+    
+    # Get best clustering
+    best_cluster = np.argmax(values)
+    return clusterings[:, best_cluster]
+
+
+
+def silhouette_multiple_clusterings(X, clusterings, num_min_samples = 5):
+    num_samples, num_features = X.shape
+    assert len(X.shape) == 2
+    assert len(clusterings.shape) == 2
+
+    # each row in clusterings is a different approach
+    assert clusterings.shape[0] == num_samples
+
+    # Get valid clusterings
+    useful = (clusterings.max(0) - clusterings.min(0)) > 0
+
+    # get distances
+    Dist = np.sqrt(((X[:, np.newaxis] - X[np.newaxis]) ** 2).sum(-1))
+
+    # Intialize best score (worst score of valid clustering is -1)
+    values = - 1.1 * np.ones(clusterings.shape[1]) 
+
+    for i in range(clusterings.shape[1]):
+        if not useful[i]:
+            continue
+
+        test_labels = clusterings[:,i]
+        num_noise_samples = (test_labels == -1).sum()
+        silhouette_labels = test_labels.copy()
+        silhouette_labels[test_labels == -1] = test_labels.max() + 1 + np.arange(num_noise_samples)
+
+        # Treat noise as separate cluster
+        test_score_noise_separate = silhouette_score(Dist, silhouette_labels, metric = 'precomputed')
+        test_score_noise_combined = silhouette_score(Dist, test_labels, metric = 'precomputed')
+
+        noise_fac = num_noise_samples / len(X)
+        values[i] = noise_fac * test_score_noise_separate + (1 - noise_fac) * test_score_noise_combined
+    
+    # Get best clustering
+    best_cluster = np.argmax(values)
+    return clusterings[:, best_cluster]
+
 
 
 class OPTICS_GMM():
@@ -21,7 +173,7 @@ class OPTICS_GMM():
     a Gaussian Multi Mixture model (GMM), according to which one can calcualte
     probability density values or sample from.
     '''
-    def __init__(self, use_cluster = True, use_PCA = True, 
+    def __init__(self, use_cluster = 'silhouette', use_PCA = True, 
                  use_std = True, estimator = 'KDE', min_std = 0.1):
         self.fitted = False
         
@@ -31,6 +183,7 @@ class OPTICS_GMM():
         self.use_std     = use_std
         self.estimator   = estimator
 
+        assert self.use_cluster in ['silhouette', 'DBCV', 'None'], "Cluster method not recognized"
         assert self.estimator in ['KDE', 'GMM', 'KNN'], "Estimator not recognized"
         
         # Get minimum std values
@@ -55,29 +208,32 @@ class OPTICS_GMM():
         self.num_features = X.shape[1]
         
         if clusters is None:
-            if self.use_cluster and len(X) >= 5:  
-                num_min_samples = X.shape[0] / 20
+            if (self.use_cluster == 'None') and len(X) >= 5:  
+                num_min_samples = X.shape[0] * self.num_features / 400 
                 num_min_samples = int(np.clip(num_min_samples, min(5, X.shape[0]), 20))     
 
                 # Get reachability plot
-                optics = OPTICS(min_samples = num_min_samples, 
-                                min_cluster_size = 5)
+                optics = OPTICS(min_samples = num_min_samples) 
                 optics.fit(X)
                     
                 reachability = optics.reachability_[np.isfinite(optics.reachability_)] 
                 
+                # Potential plotting
                 # Test potential cluster extractionssomething like
-                self.Eps = np.linspace(reachability.min(), reachability.max(), 100)
+                self.Eps = np.linspace(0, 1, 100) ** 2
+                self.Eps = self.Eps * (reachability.max() - reachability.min()) + reachability.min()
                 self.Xi = np.linspace(0.01, 0.99, 99)
 
                 Method = np.repeat(np.array(['Eps', 'Xi']), (len(self.Eps), len(self.Xi)))
                 Params = np.concatenate((self.Eps, self.Xi), axis = 0)
-
-                # Intialize best score (worst score of valid clustering is -1)
-                best_score = -1.1
+                
+                # Initializes empty clusters
+                Clustering = np.zeros((len(X), len(Method)), int)
 
                 # Iterate over all potential cluster extractions
-                for method, param in zip(Method, Params):
+                for i in range(len(Method)):
+                    method = Method[i]
+                    param  = Params[i]
                     # Cluster using dbscan
                     if method == 'Eps':
                         eps = param
@@ -89,12 +245,12 @@ class OPTICS_GMM():
                     elif method == 'Xi':
                         xi = param
                         test_labels, _ = cluster_optics_xi(reachability           = optics.reachability_,
-                                                        predecessor            = optics.predecessor_,
-                                                        ordering               = optics.ordering_,
-                                                        min_samples            = num_min_samples,
-                                                        min_cluster_size       = 2,
-                                                        xi                     = xi,
-                                                        predecessor_correction = optics.predecessor_correction)
+                                                           predecessor            = optics.predecessor_,
+                                                           ordering               = optics.ordering_,
+                                                           min_samples            = num_min_samples,
+                                                           min_cluster_size       = 2,
+                                                           xi                     = xi,
+                                                           predecessor_correction = optics.predecessor_correction)
                     else:
                         raise ValueError('Clustering method not recognized')    
                     
@@ -102,21 +258,19 @@ class OPTICS_GMM():
                     if len(np.unique(test_labels)) > 1: 
                         # Check if there are lusters of size one
                         test_clusters, test_size = np.unique(test_labels, return_counts = True)
-                        cluster_size_one = test_size[test_clusters >= 0].min() < 2
-                        
-                        # Avoid clusters consisting of only one sample
-                        if not cluster_size_one:
-                            # Evaluate clustering
-                            test_score = silhouette_score(X, test_labels)
 
-                            # Check for improvement
-                            if test_score > best_score:
-                                best_score = test_score
-                                self.cluster_labels = test_labels
-                
-                # If no viable clustering method was found
-                if not hasattr(self, 'cluster_labels'):
-                    self.cluster_labels = np.zeros(len(X))
+                        noise_clusters = test_clusters[test_size == 1]
+                        test_labels[np.isin(test_labels, noise_clusters)] = -1
+                        test_labels[test_labels > -1] = np.unique(test_labels[test_labels > -1], return_inverse = True)[1]
+
+                        Clustering[:, i] = test_labels
+
+                if self.use_cluster == 'silhouette':
+                    self.cluster_labels = silhouette_multiple_clusterings(X, Clustering)
+                elif self.use_cluster == 'DBCV':
+                    self.cluster_labels = DBCV_multiple_clusterings(X, Clustering, optics)  
+                else:
+                    raise ValueError('Clustering method not recognized')
             else:
                 self.cluster_labels = np.zeros(len(X))
         else:
