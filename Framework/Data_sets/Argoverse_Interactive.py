7ba51c6b2c10364ac152cd28a9270d1aec23754a
Julian Schumann <julian.f.schumann@web.de>
2024-08-14 13:23:17 +0200

    Adjust scene graph columns assumption

--- Framework/Data_sets/Argoverse_Interactive.py
+++ Framework/Data_sets/Argoverse_Interactive.py
@@ -139,8 +139,8 @@ class Argoverse_Interactive(data_set_template):
 
         # Prepare the SceneGraph
         self.map_split_save = True
-        sceneGraph_columns = ['ctrs', 'num_nodes', 'feats', 'centerlines', 'left_boundaries', 'right_boundaries', 'pre', 'suc', 
-                           'lane_idcs', 'pre_pairs', 'suc_pairs', 'left_pairs', 'right_pairs', 'left', 'right']  
+        sceneGraph_columns = ['num_nodes', 'lane_idcs', 'pre_pairs', 'suc_pairs', 'left_pairs', 'right_pairs',
+                              'left_boundaries', 'right_boundaries', 'centerlines']   
         self.SceneGraphs = pd.DataFrame(np.zeros((0, len(sceneGraph_columns)), object), index = [], columns = sceneGraph_columns)
 
         for _, name in tqdm(enumerate(os.listdir(file_path + '/train'))):
--- Framework/Data_sets/data_set_template.py
+++ Framework/Data_sets/data_set_template.py
@@ -389,9 +389,9 @@ class data_set_template():
                         np.save(sceneGraph_file, sceneGraph_data)
                         
                         # reset self SceneGraphs
-                        sceneGrap_columns = ['ctrs', 'num_nodes', 'feats', 'centerlines', 'left_boundaries', 'right_boundaries', 'pre', 'suc', 
-                                             'lane_idcs', 'pre_pairs', 'suc_pairs', 'left_pairs', 'right_pairs', 'left', 'right']   
-                        self.SceneGraphs = pd.DataFrame(np.zeros((0, len(sceneGrap_columns)), object), index = [], columns = sceneGrap_columns)
+                        sceneGraph_columns = ['num_nodes', 'lane_idcs', 'pre_pairs', 'suc_pairs', 'left_pairs', 'right_pairs',
+                                              'left_boundaries', 'right_boundaries', 'centerlines']   
+                        self.SceneGraphs = pd.DataFrame(np.zeros((0, len(sceneGraph_columns)), object), index = [], columns = sceneGraph_columns)
 
         if last:
             self.saved_last_orig_paths = True
