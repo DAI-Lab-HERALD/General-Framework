eb926c2f8d2ad9bb2b6e5c40f548ef398ff31ebc
Julian Schumann <julian.f.schumann@web.de>
2024-09-25 14:53:08 +0200

    Update Collision_rate_joint.py

--- Framework/Evaluation_metrics/Collision_rate_joint.py
+++ Framework/Evaluation_metrics/Collision_rate_joint.py
@@ -25,18 +25,18 @@ class Collision_rate_joint(evaluation_template):
         # Shape of Path_true:  (num_samples, 1, num_pred_agents, n_O, 2)
         # Shape of Path_pred:  (num_samples, P, num_pred_agents, n_O, 2)
         # Shape of Pred_steps: (num_samples, num_pred_agents, n_O)
-        # Shape of Type_pred:  (num_samples, num_pred_agents)
+        # Shape of Size_pred:  (num_samples, num_pred_agents, 2)
 
         # Positions where Pred_steps == Flase, Path_true and Path_pred are 0.0
 
         # Shape of Path_other: (num_pred_agents, 1, num_other_agents, n_O, 2)
-        # Shape of Type_other: (num_pred_agents, num_other_agents)
+        # Shape of Size_other: (num_pred_agents, num_other_agents, 2)
 
         # Check for each predicted agent against the ground truth of other agents
         pred_sample, pred_agent = np.where(Pred_steps.any(-1)) # Assume there are N pred agents
 
         Path_pred_agent = Path_pred[pred_sample, :, pred_agent] # Shape: (N, P, n_O, 2)
-        Type_pred_agent = Type_pred[pred_sample, pred_agent] # Shape: (N,)
+        Size_pred_agent = Size_pred[pred_sample, pred_agent] # Shape: (N, 2)
         N = len(Path_pred_agent)
         P = Path_pred_agent.shape[1]
         num_agents = Path_other.shape[1]
@@ -49,23 +49,23 @@ class Collision_rate_joint(evaluation_template):
         # Apply the indexing to the ground truth of other agents
         Path_pred_other = Path_true[pred_sample].transpose(0,2,1,3,4) # Shape: (N, num_pred_agents, P, n_O, 2)
         Path_pred_other = Path_pred_other[Idx_sample, Idx_agents] # Shape: (N, num_pred_agents - 1, P, n_O, 2)
-        Type_pred_other = Type_pred[pred_sample][Idx_sample, Idx_agents] # Shape: (N, num_pred_agents - 1)
+        Size_pred_other = Size_pred[pred_sample][Idx_sample, Idx_agents] # Shape: (N, num_pred_agents - 1, 2)
 
         Path_other = np.repeat(Path_other[pred_sample, :, :Path_pred_other.shape[-2]].transpose(0,2,1,3,4), P, axis = 2)  # Shape: (N, num_other_agents, P, n_O, 2)
-        Type_other = Type_other[pred_agent] # Shape: (N, num_other_agents)
+        Size_other = Size_other[pred_agent] # Shape: (N, num_other_agents, 2)
 
         # Concatenate the ground truth of other agents
         Path_other = np.concatenate([Path_pred_other, Path_other], axis = 1).transpose(0,2,1,3,4) # Shape: (N, num_agents, P, n_O, 2)
-        Type_other = np.concatenate([Type_pred_other, Type_other], axis = 1) # Shape: (N, num_agents)
+        Size_other = np.concatenate([Size_pred_other, Size_other], axis = 1) # Shape: (N, num_agents, 2)
 
         # Fit range so that types are N, P, num agents, and paths are N, P, num agents, n_O, 2
-        Path_pred_agent = np.repeat(Path_pred_agent[:,:,np.newaxis], num_agents, axis = 2)
-        Type_pred_agent = np.repeat(np.repeat(Type_pred_agent[:,np.newaxis,np.newaxis], P, axis = 1), num_agents, axis = 2)
+        Path_pred_agent = np.repeat(Path_pred_agent[:,:,np.newaxis], num_agents, axis = 2) # Shape: (N, P, num_agents, n_O, 2)
+        Size_pred_agent = np.repeat(np.repeat(Size_pred_agent[:,np.newaxis,np.newaxis], P, axis = 1), num_agents, axis = 2) # Shape: (N, P, num_agents, 2)
 
-        Type_other = np.repeat(Type_other[:,np.newaxis], P, axis = 1)
+        Size_other = np.repeat(Size_other[:,np.newaxis], P, axis = 1) # Shape: (N, P, num_agents, 2)
 
         # Compute the collision rate
-        Collided = self._check_collisions(Path_pred_agent, Path_other, Type_pred_agent, Type_other) # Shape: (N, P, num_agents)
+        Collided = self._check_collisions(Path_pred_agent, Path_other, Size_pred_agent, Size_other) # Shape: (N, P, num_agents)
 
         # A single collision with other agent is enough to count as a collision
         Collided = Collided.any(-1) # Shape: (N, P)
