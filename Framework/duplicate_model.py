f6b7a420d76d0470a24a9dca9d7f107da8b1210f
Julian Schumann <julian.f.schumann@web.de>
2026-02-17 11:47:59 +0100

    Update duplicate_model.py

--- Framework/duplicate_model.py
+++ Framework/duplicate_model.py
@@ -5,13 +5,13 @@ import shutil
 path = os.sep.join(os.path.dirname(os.path.realpath(__file__)).split(os.sep)) + os.sep + 'Results' + os.sep 
 
 
-# Define replacements to look for
-old = 'Pertubation_000'
-new = 'Pertubation_013'
+# Define replacement strings to look for
+old = '...'
+new = '...'
 
 # Add certain parts (to prevent accidental doubbleings)
-prefix = '--'
-suffix = '--agents_0--identi_split_0_pert=0--t_pp_old_0_0'
+prefix = '...'
+suffix = '...'
 
 old_term = prefix + old + suffix
 new_term = prefix + new + suffix
@@ -19,17 +19,22 @@ new_term = prefix + new + suffix
 data_set_folders = os.listdir(path)
 for data_set_folder in data_set_folders: 
     folder_path = path + data_set_folder + os.sep + 'Models' + os.sep
+    if not os.path.exists(folder_path):
+        continue
     model_files = os.listdir(folder_path)
     for model_file in model_files:
+        if old_term not in model_file:
+            continue
         model_file_split = model_file.split(old_term)
         if len(model_file_split) > 1:
             model_file_new = new_term.join(model_file_split)
-            if model_file_new in model_files:
-                continue
-            
             old_file = folder_path + model_file
             new_file = folder_path + model_file_new
-            if os.path.isfile(new_file):
+            if os.path.exists(new_file):
                 print('Model already exists.')
             else:
-                shutil.copyfile(old_file, new_file)
+                if os.path.isfile(old_file):
+                    shutil.copyfile(old_file, new_file)
+                else:
+                    # Copy the whole folder
+                    shutil.copytree(old_file, new_file)
